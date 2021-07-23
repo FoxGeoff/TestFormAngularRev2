e9fae6302f8b2b4df24a72a853ccd0e0e51ae10c
Geoff Fox <fox.geoffrey@gmail.com>
2021-07-22 22:50:57 -0700

    Add material module

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 2. Add material module
 3. Add @angular/forms
 4. Restart server
+
+### Task: Add Angular flex layout
+
+1. ref <https://github.com/angular/flex-layout>
+2. run ```npm i -s @angular/flex-layout @angular/cdk```
+3. Restart server
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
